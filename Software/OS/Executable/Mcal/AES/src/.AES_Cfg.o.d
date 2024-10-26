ee0ef3bfa65020cd327634672d04a96709c53d30
Mahmoud Ismail <maahmoudismaill@gmail.com>
2024-10-26 18:32:18 +0300

    Minor Changes

--- Software/OS/Executable/Mcal/AES/src/.AES_Cfg.o.d
+++
@@ -1,2 +0,0 @@
-../Executable/Mcal/AES/src/AES_Cfg.o :	../../TC399x/HSM/Mcal/AES/src/AES_Cfg.c
-../../TC399x/HSM/Mcal/AES/src/AES_Cfg.c :
--- Software/OS/Executable/Mcal/CMAC/src/.Cmac_cfg.o.d
+++
@@ -1,2 +0,0 @@
-../Executable/Mcal/CMAC/src/Cmac_cfg.o :	../../TC399x/HSM/Mcal/CMAC/src/Cmac_cfg.c
-../../TC399x/HSM/Mcal/CMAC/src/Cmac_cfg.c :
